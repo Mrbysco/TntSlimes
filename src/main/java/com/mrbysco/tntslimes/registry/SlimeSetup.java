8f91e3779b40578698a8a2e5188c3384b4244f17
Mrbysco <Mrbysco@users.noreply.github.com>
2024-07-07 22:01:53 +0200

    Update neoforge support

--- src/main/java/com/mrbysco/tntslimes/registry/SlimeSetup.java
+++ src/main/java/com/mrbysco/tntslimes/registry/SlimeSetup.java
@@ -5,15 +5,15 @@ import net.minecraft.world.entity.SpawnPlacementTypes;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
-import net.neoforged.neoforge.event.entity.SpawnPlacementRegisterEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 public class SlimeSetup {
 	public static void registerEntityAttributes(EntityAttributeCreationEvent event) {
 		event.put(SlimeRegistry.TNT_SLIME.get(), Monster.createMonsterAttributes().build());
 	}
 
-	public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
+	public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event) {
 		event.register(SlimeRegistry.TNT_SLIME.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-				TNTSlime::checkTNTSlimeSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+				TNTSlime::checkTNTSlimeSpawnRules, RegisterSpawnPlacementsEvent.Operation.AND);
 	}
 }
